6d34850967a182d1d857cd4978d0555858d718a7
andraandronic <andraandronic61@yahoo.com>
2019-05-12 19:34:42 +0300

    Am populat  baza de date pentru resources

---
+++ HOME/resource.php
@@ -0,0 +1,75 @@
+insert into resources values(1,"Algorithm Design","trex/Resursa.php?id=1/");
+insert into resources values(2,"The Algorithm Design Manual","trex/Resursa.php?id=2/");
+insert into resources values(3,"Principles of Algorithm Design","trex/Resursa.php?id=3/");
+insert into resources values(4,"A complexity theory of efficient parallel algorithms","trex/Resursa.php?id=4/");
+insert into resources values(5,"Algorithmic Complexity","trex/Resursa.php?id=5/");
+insert into resources values(6,"On the Sphere-Decoding Algorithm I. Expected Complexity","trex/Resursa.php?id=6/");
+insert into resources values(7,"Probabilistically Checkable Proofs and Hardness of Approximation","trex/Resursa.php?id=7/");
+insert into resources values(8,"Complexity and Reductions","trex/Resursa.php?id=8/");
+insert into resources values(9,"Understanding NP Complete and NP Hard Problems","trex/Resursa.php?id=9/");
+insert into resources values(10,"Software Requirements","trex/Resursa.php?id=10/");
+insert into resources values(11,"Safety Analysis of Software Requirements: Model and Process","trex/Resursa.php?id=11/");
+insert into resources values(12,"Different Types of Software Requirement - Functional, Non-Functional - SE","trex/Resursa.php?id=12/");
+insert into resources values(13,"Software Design Basics","trex/Resursa.php?id=13/");
+insert into resources values(14,"Software design description","trex/Resursa.php?id=14/");
+insert into resources values(15,"SOFTWARE DESIGN","trex/Resursa.php?id=15/");
+insert into resources values(16,"Software Constraction","trex/Resursa.php?id=16/");
+insert into resources values(17,"SOFTWARE CONSTRUCTION","trex/Resursa.php?id=17/");
+insert into resources values(18,"Software Construction","trex/Resursa.php?id=18/");
+insert into resources values(19,"Software Testing Tutorial: Free Course","trex/Resursa.php?id=19/");
+insert into resources values(20,"Software Testing Training","trex/Resursa.php?id=20/");
+insert into resources values(21,"The Art of Software Testing,Second Edition","trex/Resursa.php?id=21/");
+insert into resources values(22,"Software Maintenance Overview","trex/Resursa.php?id=22/");
+insert into resources values(23,"Software Maintenance","trex/Resursa.php?id=23/");
+insert into resources values(24,"What is Software Maintenance","trex/Resursa.php?id=24/");
+insert into resources values(25,"SOFTWARE ENGINEERING MANAGEMENT","trex/Resursa.php?id=25/");
+insert into resources values(26,"What software engineering management is like","trex/Resursa.php?id=26/");
+insert into resources values(27,"Software Engineering Management","trex/Resursa.php?id=27/");
+insert into resources values(28,"Functional Programming - Introduction","trex/Resursa.php?id=28/");
+insert into resources values(29,"Functional Programming Paradigm","trex/Resursa.php?id=29/");
+insert into resources values(30,"Functional Programming Fundamentals","trex/Resursa.php?id=30/");
+insert into resources values(31,"How to explain object-oriented programming concepts to a 6-year-old","trex/Resursa.php?id=31/");
+insert into resources values(32,"Basic Concepts of Object Oriented Programming using C++","trex/Resursa.php?id=32/");
+insert into resources values(33,"What is Object Oriented Programming (OOPS)? Simple Explanation for Beginners","trex/Resursa.php?id=33/");
+insert into resources values(34,"Introduction to Software Paradigms and Procedural Programming Paradigm","trex/Resursa.php?id=34/");
+insert into resources values(35,"First Year Computing Course: An introduction to procedural programming","trex/Resursa.php?id=35/");
+insert into resources values(36,"Procedural programming paradigm","trex/Resursa.php?id=36/");
+insert into resources values(37,"Web Application Security—Past, Present, and Future","trex/Resursa.php?id=37/");
+insert into resources values(38,"WEB APPLICATION SECURITY","trex/Resursa.php?id=38/");
+insert into resources values(39,"Web Application Security","trex/Resursa.php?id=39/");
+insert into resources values(40,"Mobile Application Security: Who, How and Why","trex/Resursa.php?id=40/");
+insert into resources values(41,"MOBILE APPLICATION SECURITY","trex/Resursa.php?id=41/");
+insert into resources values(42,"Mobile App Security","trex/Resursa.php?id=42/");
+insert into resources values(43,"Network Security and Types of Attacks in Network","trex/Resursa.php?id=43/");
+insert into resources values(44,"Fundamentals of Network Security","trex/Resursa.php?id=44/");
+insert into resources values(45,"Network Security Tutorial | Introduction to Network Security | Network Security Tools","trex/Resursa.php?id=45/");
+insert into resources values(46,"Reverse Engineering for Beginners (Understanding Assembly Language)","trex/Resursa.php?id=46/");
+insert into resources values(47,"Introduction to Reverse Engineering","trex/Resursa.php?id=47/");
+insert into resources values(48,"Reverse Engineering Basics","trex/Resursa.php?id=48/");
+insert into resources values(49,"Front-End Developer Handbook","trex/Resursa.php?id=49/");
+insert into resources values(50,"Front-End Developer Handbook","trex/Resursa.php?id=50/");
+insert into resources values(51,"The 2019 Frontend Developer Crash Course - HTML and CSS Tutorial for Beginners","trex/Resursa.php?id=51/");
+insert into resources values(52,"The Web Communication Protocols","trex/Resursa.php?id=52/");
+insert into resources values(53,"Internet Protocols","trex/Resursa.php?id=53/");
+insert into resources values(54,"Network Protocols and the Internet","trex/Resursa.php?id=54/");
+insert into resources values(55,"MICROSOFT WINDOWS OPERATING SYSTEM","trex/Resursa.php?id=55/");
+insert into resources values(56,"Windows 10","trex/Resursa.php?id=56/");
+insert into resources values(57,"Deploying and managing Windows Information Protection - BRK3011","trex/Resursa.php?id=57/");
+insert into resources values(58,"Mac OS","trex/Resursa.php?id=58/");
+insert into resources values(59,"Mac OS History","trex/Resursa.php?id=59/");
+insert into resources values(60,"Everything New in macOS Mojave","trex/Resursa.php?id=60/");
+insert into resources values(61,"Linux operating system","trex/Resursa.php?id=61/");
+insert into resources values(62,"Introduction to the Linux Operating System","trex/Resursa.php?id=62/");
+insert into resources values(63,"Introduction to Linux and Basic Linux Commands for Beginners","trex/Resursa.php?id=63/");
+insert into resources values(64,"What Everybody Ought to Know About Android : Introduction, Features and Applications","trex/Resursa.php?id=64/");
+insert into resources values(65,"Android Operating System: Architecture, Security Challenges and Solutions","trex/Resursa.php?id=65/");
+insert into resources values(66,"The History of Android - Versions, Features and Origins","trex/Resursa.php?id=66/");
+insert into resources values(67,"What Is the iPhone OS (iOS)","trex/Resursa.php?id=67/");
+insert into resources values(68,"INTRODUCTION TO IOS","trex/Resursa.php?id=68/");
+insert into resources values(69,"History of iOS","trex/Resursa.php?id=69/");
+
+
+
+
+
+
