04ce65156c32f66625dbe8b625625a1704016456
Calancea Bianca <calanceabianca15@gmail.com>
2019-06-11 22:44:08 +0300

    add and delete bookmark

--- HOME/bookmarks.php
+++ HOME/bookmarks.php
@@ -4,11 +4,33 @@ function getBookmarks($conn,$id){
  $sql="SELECT resources.name as name FROM resources join bookmarks on bookmarks.resourceId=resources.id 
  join users on bookmarks.userId=users.id where bookmarks.userId={$id}";
  $result = $conn->query($sql);
-
+ echo "<ol>";
  while($row = $result->fetch_assoc()){
-         echo "<a>".$row['name']."</a><br>";
+         echo "<li><a>".$row['name']."</a></li><br>";
      }
+     echo "</ol>";
      
  }
 
+
+ function setBookmarks($conn,$id){
+ 
+    if(isset($_POST['bookmarkSubmit'])){
+        $uid=$_POST['userId'];
+        $sql="INSERT INTO bookmarks(userId ,resourceId) VALUES  ('$uid','$id')";
+        $result = $conn->query($sql);
+      }
+    
+    }
+
+    function deleteBookmarks($conn,$id){
+ 
+        if(isset($_POST['bookmarkSubmit'])){
+            $uid=$_POST['userId'];
+            $sql="DELETE from bookmarks where userId={$uid} and resourceId={$id} ";
+            $result = $conn->query($sql);
+          }
+        
+        }
+   
 ?>
